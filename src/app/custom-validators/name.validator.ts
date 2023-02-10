48e56f297b097bc7929ed841ead0bc082dd622bc
tirupathirao0201 <tirupathirao@divami.com>
2023-02-10 20:01:42 +0530

    'namevalidatoradded'

--- src/app/custom-validators/name.validator.ts
+++ src/app/custom-validators/name.validator.ts
@@ -1,13 +1,18 @@
 import {AbstractControl} from '@angular/forms';
 
 export function NameError(control:AbstractControl){
+    const pattern = /^[a-zA-Z]+/;
     if (control.value!=null && control.value != ''){
+
+        if(! pattern.test(control.value))
+        return {error:true,msg:'only alphabets accepted'}
 
         if(control.value.length<3)
             return {error:true,msg:'minimum length of name should be atleast 3 character'}
         
         if (control.value>256)
             return {error:true,msg:'maximum length of name should be atmost 256 characters'}
+ 
         
         return null;   
     }
